a7746a4a095c38780c2ff0d9fbd3fd9f228f9995
Emanuel Schemberger <emanueldev24@gmail.com>
2022-01-21 18:16:46 -0300

    change 2

---
+++ 3-grid.py
@@ -0,0 +1,30 @@
+import random 
+
+class Grid(object):
+    'representation of a 2d array'
+    def __init__(self, rows, columns, fill_value = None):
+        self.data = Array(rows)
+        for row in range(rows):
+            self.data[row] = Array(columns, fill_value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def __addRandomItems__(self, lower, upper):
+        self.items = [random.randint(lower, upper)
+                      for i in range(len(self.items))]
+
